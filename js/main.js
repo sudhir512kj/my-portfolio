254653f5434c8715fe426414057e32b6c0fa674b
sudhir meena <sureshmeena512@gmail.com>
2018-06-18 16:30:10 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function hireme(){
+    alert("to hire me...mail me at  sureshmeena512@gmail.com");
+}
+
+WebFont.load({
+    google: {
+        families: ['Dancing Script', 'Sofia']
+    }
+    });
+
